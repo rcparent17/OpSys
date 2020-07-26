0108af567060660761def4584752485c55e8c929
rcparent17 <reilly.c.parent@gmail.com>
2020-07-25 21:23:11 -0400

    finished lab 1

--- lab1/myshell.c
+++ lab1/myshell.c
@@ -1,45 +1,83 @@
+// myshell.c
+// written by Reilly Parent
+
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 
+// the main function. starts the shell and handles commands
 int main(void){
 
-	char inBuffer[200];
+	// input buffer setup
+	int maxBuffLen = 200;
+	char inBuffer[maxBuffLen];
+
+	// pointer to hold tokens generated from strtok()
 	char* currToken;
+	
+	// variable to keep track of the nnumber of arguments in a command
 	int numArgs = 0;
+	
+	// no one is going to have 15 arguments right?
 	char* argBuffer[15];
+	argBuffer[0] = ""; // initialize in case the first command is empty
+	
+	//pid variable for fork()
+	int pid;
+	
+	// char index for replacing EOF and \n in commands
+	int charIdx;
 
-	while(inBuffer != "quit\r\n"){
-
+	// forever, lol
+	while(1){
 		printf("myShell> ");
-		fgets(inBuffer, 200, stdin);
-		printf("%s\n", inBuffer);
-		currToken = strtok(inBuffer, " ");
-		while(currToken){ 
-			argBuffer[numArgs] = currToken;
-			numArgs++;
-			currToken = strtok(NULL, " ");
+
+		// get user input and store it in inBuffer
+		fgets(inBuffer, maxBuffLen, stdin);
+
+		// replace EOF and \n with null character
+		for(charIdx = 0; charIdx < maxBuffLen; charIdx++){
+			if(inBuffer[charIdx] == '\n' || inBuffer[charIdx] == EOF){
+				inBuffer[charIdx] = '\0';
+				}
 		}
 
-		char* args[numArgs++];
-		for(int i = 0; i < numArgs; i++){
-			if(i == numArgs - 1){
-				args[i] = NULL;
+		// strtok is kinda similar to split() in python. this gets the first token from the string split by a space
+		currToken = strtok(inBuffer, " ");
+		
+		//while the token isn't empty
+		while(currToken){ 
+			argBuffer[numArgs++] = currToken; // store it in the argBuffer and increment the arg counter
+			currToken = strtok(NULL, " "); // get the next token (not really sure why NULL is passed instead of inBuffer though)
+		}
+
+		// if the first argument is "quit", then quit!
+		if(strcmp(argBuffer[0], "quit") == 0){
+			printf("Goodbye!\n");
+			exit(0); // exit with status 0
+		}
+		// if the command is "sort"
+		else if(strcmp(argBuffer[0], "sort") == 0){
+
+			// start a process to execute ./mysort on argBuffer[1]
+			pid = fork();
+			if(pid == 0){
+				execlp("./mysort", "mysort", argBuffer[1], NULL);
 			}
 			else{
-				args[i] = argBuffer[i];
+				waitpid(pid, NULL, 0); // wait for the child process to end
 			}
-			//printf("%s\n", args[i]);
 		}
-		int pid = fork();
-		if(pid == 0){
-			execvp(args[0], args);
+		// if we don't know the command, tell the user!
+		else {
+			if(!strcmp(argBuffer[0], "") == 0){
+				printf("Command not known: %s\n", argBuffer[0]);
+			}
 		}
-		else{
-			wait(NULL);
-		}
-		numArgs = 0;
+		numArgs = 0; // reset the argument counter at the end of the loop
 	}
+	
 	return 0;
-
 }
